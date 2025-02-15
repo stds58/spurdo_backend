800b3c03eed48fa1e7b430b2e614553986cb85d5
Andrey <v.andrei.dmitrievich@gmail.com>
2025-02-15 13:13:16 +0300

    added swagger

--- app/app/settings.py
+++ app/app/settings.py
@@ -91,5 +91,8 @@ USE_I18N = True
 USE_TZ = False
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- app/app/urls.py
+++ app/app/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/swagger', TemplateView.as_view(template_name='swagger.html'))
 ]
